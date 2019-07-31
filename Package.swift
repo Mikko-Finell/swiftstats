5781a9195d90ffcd53256a84bf27d5cf327136d2
mikko <mikko.finell@gmail.com>
2019-07-31 13:55:02 +0300

    fork

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftStats",
+    products: [
+        .library(
+            name: "SwiftStats",
+            targets: ["SwiftStats"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftStats",
+            dependencies: []),
+    ]
+)
--- build/SwiftStats.build/Release/SwiftStats.build/Objects-normal/x86_64/SwiftStats_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/matthew/Desktop/swiftstats/build/SwiftStats.build/Release/SwiftStats.build/DerivedSources/SwiftStats_vers.c
